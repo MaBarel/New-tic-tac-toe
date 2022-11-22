77fa3fa8c6c243ca9bd181b78b2cec82bcd59f96
BenDoverMarij <115532706+MaBarel@users.noreply.github.com>
2022-11-22 12:57:16 +0100

    for now

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 console.log("main loaded");
+//variables
+const X_Class = "blockx";
+const O_Class = "blocko";
+const blockElement = document.querySelectorAll ('[block]')
+//click function
+blockElement.forEach(cell =>{
+    cell.addEventListener('click', clickPressed, { once: true})
+})
