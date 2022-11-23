42e8dd996754f09cbbaaa76045cceccd3576261d
BenDoverMarij <115532706+MaBarel@users.noreply.github.com>
2022-11-24 00:38:47 +0100

    mostly done

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,47 @@
 console.log("functions loaded");
+
+function clickPressed(e){
+    const block = e.target
+    const XorO = turn ? O_Class : X_Class
+    console.log("clicked")
+    placeMark(block, XorO)
+
+    checkWinner();
+    flip();
+}
+
+function placeMark(block, XorO){
+    block.classList.add(XorO)
+} 
+
+function flip(){
+    turn = !turn
+}
+
+function checkWinner() {
+    for (let i = 0; i < winningNumbas.length; i++) {
+       const winnings = winningNumbas[i];
+       const no1 = winnings[0];
+       const no2 = winnings[1];
+       const no3 = winnings[2];
+
+       const check1 = blockElement[no1];
+       const check2 = blockElement[no2];
+       const check3 = blockElement[no3];
+
+       console.log('new check');
+       console.log(check1);
+       console.log(check2);
+       console.log(check3);
+    
+       //TODO check that check1, check2 and check3 have the sameclasses
+       if(check1.classList.contains(X_Class) && check2.classList.contains(X_Class) && check3.classList.contains(X_Class)){
+        winningText.innerHTML = "X wins";
+        winningMessage.classList.add("show");
+       } else if(check1.classList.contains(O_Class) && check2.classList.contains(O_Class) && check3.classList.contains(O_Class)){
+        winningText.innerHTML = "O wins";
+        winningMessage.classList.add("show")
+       }
+    }
+   }
+
--- js/main.js
+++ js/main.js
@@ -2,8 +2,30 @@ console.log("main loaded");
 //variables
 const X_Class = "blockx";
 const O_Class = "blocko";
-const blockElement = document.querySelectorAll ('[block]')
+const winningNumbas = [
+[0,1,2],
+[3,4,5],
+[6,7,8],
+[0,3,6],
+[1,4,7],
+[2,5,8],
+[0,4,8],
+[2,4,6]
+];
+const winningText = document.querySelector(".winningText");
+const winningMessage = document.querySelector(".winningMessage")
+const blockElement = document.querySelectorAll('.block');
+const restartButton = document.querySelector(".winningMessagebutton")
+let turn;
 //click function
-blockElement.forEach(cell =>{
-    cell.addEventListener('click', clickPressed, { once: true})
-})
+blockElement.forEach(block =>{
+    block.addEventListener('click', clickPressed, { once: true})
+});
+
+restartButton.addEventListener("click", pressed);
+
+function pressed(){
+    location.reload();
+}
+
+
